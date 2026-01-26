5104eb080648003b838d5c59e4d773a966ea93ae
vinoth-kumar-v <91128089+vinoth-xxii@users.noreply.github.com>
2026-01-26 20:42:35 +0530

    Time: 5 ms (50.55%) | Memory: 46.1 MB (98.58%) - LeetSync

--- 1-two-sum/two-sum.java
+++ 1-two-sum/two-sum.java
@@ -1,14 +1,17 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        //let just try first with brute force
-        int arr_len = nums.length;
-        for(int i = 0; i < arr_len - 1; i++) { //don't want to check last
-            for(int j = i + 1; j < arr_len; j++){
-                if(nums[i] + nums[j] == target){
-                    return new int[]{i,j};
-                }
-            }
-        }
-        return null;
+    //by using the 2nd hint, if i do subtract target - x = y
+    //what will be time complexity of finding the y?
+    //from example 2; 6 - 3's 3 and is not present(how to effectly find this)
+    int arr_len = nums.length;
+
+    HashMap<Integer, Integer> dict = new HashMap<>();
+    for(int i = 0; i < arr_len; i++) dict.put(nums[i],i); //key, value ~ data,index
+    int y;
+    for(int i = 0; i<arr_len - 1; i++){
+        y = target - nums[i];
+        if(dict.containsKey(y) && dict.get(y) != i) return new int[]{dict.get(y),i}; 
+    }
+    return null;
     }
 }
