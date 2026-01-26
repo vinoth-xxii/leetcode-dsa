7528a7eafaf4573bf1f7833fd81b7eebc594b6a1
vinoth-kumar-v <91128089+vinoth-xxii@users.noreply.github.com>
2026-01-26 18:14:02 +0530

    Time: 39 ms (37.01%) | Memory: 47 MB (70.55%) - LeetSync

--- 1-two-sum/two-sum.java
+++ 1-two-sum/two-sum.java
@@ -2,7 +2,7 @@ class Solution {
     public int[] twoSum(int[] nums, int target) {
         //let just try first with brute force
         int arr_len = nums.length;
-        for(int i = 0; i < arr_len - 1; i++) { //don't want i to check last
+        for(int i = 0; i < arr_len - 1; i++) { //don't want to check last
             for(int j = i + 1; j < arr_len; j++){
                 if(nums[i] + nums[j] == target){
                     return new int[]{i,j};
