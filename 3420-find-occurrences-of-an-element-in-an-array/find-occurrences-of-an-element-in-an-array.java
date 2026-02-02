999a5d4c90f2cc8648496e0d8fe5178020040efc
Vinoth Kumar V <91128089+vinoth-xxii@users.noreply.github.com>
2026-02-02 14:14:19 +0530

    Time: 10 ms (39.96%) | Memory: 171.1 MB (58.07%) - LeetSync

---
+++ 3420-find-occurrences-of-an-element-in-an-array/find-occurrences-of-an-element-in-an-array.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int[] occurrencesOfElement(int[] nums, int[] queries, int x) {
+        HashMap<Integer, Integer> hashmap = new HashMap(); //O(N) space
+        int[] answer = new int[queries.length];   //O(M) space
+        int count = 0;
+        for (int i = 0; i < nums.length; i++) {   //O(N) time 
+            if (nums[i] == x)
+                hashmap.put(++count, i);
+        }
+        for (int i = 0; i < queries.length; i++) { //O(M) time 
+            if (queries[i] <= count) {
+                answer[i] = hashmap.get(queries[i]);  //O(1) time
+            } else
+                answer[i] = -1;
+        }
+        return answer;
+    }
+}
