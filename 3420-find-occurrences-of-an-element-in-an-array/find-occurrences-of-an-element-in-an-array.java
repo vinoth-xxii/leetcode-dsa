fd7f96fd8676cb9ec23b7fed2545e781a5a07640
Vinoth Kumar V <91128089+vinoth-xxii@users.noreply.github.com>
2026-02-02 14:54:56 +0530

    Time: 5 ms (84.23%) | Memory: 162.7 MB (86.74%) - LeetSync

--- 3420-find-occurrences-of-an-element-in-an-array/find-occurrences-of-an-element-in-an-array.java
+++ 3420-find-occurrences-of-an-element-in-an-array/find-occurrences-of-an-element-in-an-array.java
@@ -1,18 +1,19 @@
 class Solution {
     public int[] occurrencesOfElement(int[] nums, int[] queries, int x) {
         //Additional solution - without using HashMap
-        List<Integer> indices = new ArrayList<>();  //List creation
-        int[] answer = new int[queries.length];
+        List<Integer> indices = new ArrayList<>();  //List creation O(N) space
+        int[] answer = new int[queries.length];  //O(M) space
 
-        for(int i = 0; i < nums.length; i++){
+        for(int i = 0; i < nums.length; i++){ //O(N) time
             if(nums[i] == x) {
                 indices.add(i); //since there are asking index, storing index here
             }
         }
 
-        for(int i = 0; i < queries.length; i++){
+        for(int i = 0; i < queries.length; i++){  //O(M) time
             int occurance = queries[i];
-            if(occurance <= indices.size()){ //for list use size()
+            //added condtion for occurace when 0
+            if(occurance > 0 && occurance <= indices.size()){ //for list use size()
                 answer[i] = indices.get(occurance-1); //for list use get()
             }
             else answer[i] = -1;
