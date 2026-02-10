8571283854800cc7a2605ec5b1326f6b646fd295
Vinoth Kumar V <91128089+vinoth-xxii@users.noreply.github.com>
2026-02-10 17:07:52 +0530

    Time: 4 ms (54.16%) | Memory: 45 MB (60.01%) - LeetSync

--- 414-third-maximum-number/third-maximum-number.java
+++ 414-third-maximum-number/third-maximum-number.java
@@ -2,32 +2,27 @@ class Solution {
     public int thirdMax(int[] nums) {
         Integer first, second, third;
         first = null; second = null; third = null;
-        for(int num : nums){
-            if(first == null){
-                first = num;
-                continue;
-            }
-            if(num > first){
+        for(Integer num : nums){
+
+            // if(first == num || second == num || third == num) continue; null == num X
+            //if(first.equals(num) || second.equals(num) || third.equals(num)) continue;
+            if(num.equals(first) || num.equals(second) || num.equals(third)) continue;
+            
+            if(first == null || num > first){
                 third = second;
                 second = first;
                 first = num;
                 continue;
             }
-            if(second == null && first != num){
-                second = num;
-                continue;
-            }
-            if(second != null && num > second && first != num){
+            if(second == null || num > second && first != num){
                 third = second;
                 second = num;
                 continue;
             }
-            if(third == null && first != num && second != num){
+
+            if(third == null || num > third && first != num && second != num){
                 third = num;
                 continue;
-            }
-            if(third != null && num > third && first != num && second != num){
-                third = num;
             }
         }
         if(third != null) return third;
