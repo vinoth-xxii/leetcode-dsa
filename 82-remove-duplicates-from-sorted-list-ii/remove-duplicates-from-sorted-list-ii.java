0ece031c17af64871efb43fc70168df0688fe06f
Vinoth Kumar V <91128089+vinoth-xxii@users.noreply.github.com>
2026-02-16 21:31:16 +0530

    Time: 0 ms (100.00%) | Memory: 45.2 MB (18.05%) - LeetSync

--- 82-remove-duplicates-from-sorted-list-ii/remove-duplicates-from-sorted-list-ii.java
+++ 82-remove-duplicates-from-sorted-list-ii/remove-duplicates-from-sorted-list-ii.java
@@ -8,40 +8,33 @@
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
+
+/*just popped in my be head while looking at the picture
+  we kn we have to use two pointer, how we use it is the challenge
+  plus we could use swap logic until we get unique, plus i remember
+  passing node.next in binary tree
+*/
 class Solution {
+    public ListNode getNext(ListNode node) {
+        if (node == null) {
+            return node;
+        }
+        ListNode temp = node;
+        if (temp.next != null && temp.val == temp.next.val) { //isDuplicate
+            int dupVal = temp.val;
+            while (temp != null && temp.val == dupVal) {
+                temp = temp.next;
+            }
+            node = getNext(temp);
+        } else {
+            node.next = getNext(node.next);
+        }
+        return node;
+
+    }
+
     public ListNode deleteDuplicates(ListNode head) {
-        /* i can store the elements in hashmap
-           and check it against the val of visiting node */
-
-        // space - O(n)
-        HashMap<Integer, Integer> hash = new HashMap<>();
-        ListNode currentNode = head;
-
-        // time - 2 * O(n) = O(n)
-        while (currentNode != null) {
-            if (hash.containsKey(currentNode.val)) {
-                hash.put(currentNode.val, hash.get(currentNode.val) + 1);
-            } else {
-                hash.put(currentNode.val, 1);
-            }
-            currentNode = currentNode.next;
-        }
-
-        ListNode tempNode = head;
-
-        while (tempNode != null && hash.get(tempNode.val) > 1) {
-            head = tempNode.next;
-            tempNode = tempNode.next;
-        }
-
-        while (tempNode != null && tempNode.next != null) {
-            if (hash.get(tempNode.next.val) > 1) {
-                tempNode.next = tempNode.next.next;
-            } else {
-                tempNode = tempNode.next;
-            }
-        }
-
-        return head;
+        ListNode modHead = getNext(head);
+        return modHead;
     }
 }
