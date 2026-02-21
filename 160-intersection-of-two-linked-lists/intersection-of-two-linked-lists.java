5f48aa8b9d8165f9dfad7c6c23e0361b117f7562
Vinoth Kumar V <91128089+vinoth-xxii@users.noreply.github.com>
2026-02-21 12:24:11 +0530

    Time: 1435 ms (5.01%) | Memory: 52.8 MB (79.42%) - LeetSync

--- 160-intersection-of-two-linked-lists/intersection-of-two-linked-lists.java
+++ 160-intersection-of-two-linked-lists/intersection-of-two-linked-lists.java
@@ -11,27 +11,22 @@
  */
 public class Solution {
     public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
-        //1st i will go with hash approach
-        Set<ListNode> hashmapA = new HashSet<>();
-
-        if(headA == null || headB == null){
+        /*Approach 1 : hashSet
+          Approach 2 : two pointer - Quadratic time*/
+        if (headA == null || headB == null) {
             return null;
         }
-
-        ListNode current = headA;
-        while(current != null){
-            hashmapA.add(current);
-            current = current.next;
-        }
-
-        ListNode current1 = headB;
-        while(current1 != null){
-            if(hashmapA.contains(current1)){
-                return current1;
+        ListNode pt1 = headA;
+        while (pt1 != null) {
+            ListNode pt2 = headB;
+            while (pt2 != null) {
+                if (pt1 == pt2) {
+                    return pt1;
+                }
+                pt2 = pt2.next;
             }
-            current1 = current1.next;
+            pt1 = pt1.next;
         }
-
         return null;
     }
 }
