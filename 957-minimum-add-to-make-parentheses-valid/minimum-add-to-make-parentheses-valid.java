3159318fb6930d28ba6bae8de65f9c23c630924e
Vinoth Kumar V <91128089+vinoth-xxii@users.noreply.github.com>
2026-04-20 13:22:21 +0530

    Time: 0 ms (100.00%) | Memory: 42.9 MB (42.74%) - LeetSync

---
+++ 957-minimum-add-to-make-parentheses-valid/minimum-add-to-make-parentheses-valid.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int minAddToMakeValid(String s) {
+        int str_length = s.length();
+        int open = 0;
+        int close = 0;
+        for(int i = 0; i < str_length; i++){
+            if(s.charAt(i) == '('){
+                open++;
+            }//closed brac
+            else{
+                if(open > 0){
+                    open--;
+                }
+                else{
+                    close++;
+                }
+            }
+        }
+        
+        return close + open;
+    }
+}
