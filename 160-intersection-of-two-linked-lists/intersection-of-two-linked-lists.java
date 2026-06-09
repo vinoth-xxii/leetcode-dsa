1c77c09dedbf8b7e2ec5fedb7676010278b06611
Vinoth Kumar V <91128089+vinoth-xxii@users.noreply.github.com>
2026-06-09 08:19:06 +0530

    Time: 3 ms (19.58%) | Memory: 53.3 MB (11.64%) - LeetSync

--- 160-intersection-of-two-linked-lists/intersection-of-two-linked-lists.java
+++ 160-intersection-of-two-linked-lists/intersection-of-two-linked-lists.java
@@ -9,24 +9,39 @@
  *     }
  * }
  */
+
+/* I knew i had to traverse from last to first to get the intersecting value,
+The problem was how do i achieve it or aka how to keep track or store it without
+simply using hashmp or an array, then traverse it back. then i immediately thought of using 
+stack, which follows LIFO*/
 public class Solution {
     public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
-        /*Approach 1 : hashSet
-          Approach 2 : two pointer - Quadratic time*/
-        if (headA == null || headB == null) {
-            return null;
+        Stack<ListNode> stackA = new Stack<>();
+        Stack<ListNode> stackB = new Stack<>();
+
+        ListNode currA = headA;
+        while(currA != null){
+            stackA.push(currA);
+            currA = currA.next;
         }
-        ListNode pt1 = headA;
-        while (pt1 != null) {
-            ListNode pt2 = headB;
-            while (pt2 != null) {
-                if (pt1 == pt2) {
-                    return pt1;
-                }
-                pt2 = pt2.next;
+
+        ListNode currB = headB;
+        while(currB != null){
+            stackB.push(currB);
+            currB = currB.next;
+        }
+
+        ListNode result = null;
+        while(!stackA.isEmpty() && !stackB.isEmpty()){
+            ListNode tempA = stackA.pop();
+            ListNode tempB = stackB.pop();
+            if(tempA == tempB){
+                result = tempA;
             }
-            pt1 = pt1.next;
+            else{
+                break;
+            }
         }
-        return null;
+        return result;
     }
 }
